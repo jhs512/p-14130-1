a78c1eb5ff20f9a4063da9947b1c5753bb0d9947
jhs512 <jangka512@gmail.com>
2026-07-15 11:39:37 +0900

    003

---
+++ src/main/kotlin/com/Main.kt
@@ -0,0 +1,14 @@
+package com
+
+fun main() {
+    println("== 명언 앱 ==")
+
+    while (true) {
+        print("명령) ")
+
+        val input = readlnOrNull()
+
+        if (input == "종료")
+            break
+    }
+}
