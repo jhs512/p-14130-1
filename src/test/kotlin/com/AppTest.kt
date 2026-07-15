6aed21beb0b71bb7e761b8384ddd0e4581b1efc4
jhs512 <jangka512@gmail.com>
2026-07-16 00:43:01 +0900

    023

--- src/test/kotlin/com/AppTest.kt
+++ src/test/kotlin/com/AppTest.kt
@@ -1,8 +1,8 @@
 package com
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import kotlin.test.Test
-import kotlin.test.assertContains
 
 class AppTest {
     @Test
@@ -16,9 +16,10 @@ class AppTest {
         """
         )
         println("result: $result")
-        assertContains(result, "명언 : ")
-        assertContains(result, "작가 : ")
-        assertContains(result, "1번 명언이 등록되었습니다.")
+
+        assertThat(result).contains("명언 : ")
+        assertThat(result).contains("작가 : ")
+        assertThat(result).contains("1번 명언이 등록되었습니다.")
     }
 
     @Test
@@ -35,7 +36,27 @@ class AppTest {
         """
         )
 
-        assertContains(result, "1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
-        assertContains(result, "2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
+        assertThat(result).contains("1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
+        assertThat(result).contains("2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
+    }
+
+    @Test
+    fun `명언 삭제`() {
+        val result = TestRunner.run(
+            """
+            등록
+            나의 죽음을 적들에게 알리지 말라.
+            충무공 이순신
+            등록
+            천재는 99%의 노력과 1%의 영감이다.
+            에디슨
+            삭제?id=1
+            목록
+        """
+        )
+
+        assertThat(result).contains("1번 명언을 삭제하였습니다.")
+        assertThat(result).doesNotContain("1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
+        assertThat(result).contains("2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
     }
 }
