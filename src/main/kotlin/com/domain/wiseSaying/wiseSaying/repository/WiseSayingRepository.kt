06f3aa1a1a7e3b41b4046ae2701243eacf3c1a0c
jhs512 <jangka512@gmail.com>
2026-07-16 00:47:06 +0900

    027

--- src/main/kotlin/com/domain/wiseSaying/wiseSaying/repository/WiseSayingRepository.kt
+++ src/main/kotlin/com/domain/wiseSaying/wiseSaying/repository/WiseSayingRepository.kt
@@ -2,31 +2,11 @@ package com.domain.wiseSaying.wiseSaying.repository
 
 import com.domain.wiseSaying.wiseSaying.entity.WiseSaying
 
-class WiseSayingRepository {
-    private var lastId = 0
-    private val wiseSayings = mutableListOf<WiseSaying>()
-
-    fun save(wiseSaying: WiseSaying): WiseSaying {
-        if (wiseSaying.isNew()) {
-            wiseSaying.id = ++lastId
-            wiseSayings.add(wiseSaying)
-        }
-
-        return wiseSaying
-    }
-
-    fun isEmpty(): Boolean = wiseSayings.isEmpty()
-
-    fun findAll(): List<WiseSaying> = wiseSayings
-
-    fun findById(id: Int): WiseSaying? = wiseSayings.firstOrNull { it.id == id }
-
-    fun delete(wiseSaying: WiseSaying) {
-        wiseSayings.remove(wiseSaying)
-    }
-
-    fun clear() {
-        lastId = 0
-        wiseSayings.clear()
-    }
+interface WiseSayingRepository {
+    fun save(wiseSaying: WiseSaying): WiseSaying
+    fun isEmpty(): Boolean
+    fun findAll(): List<WiseSaying>
+    fun findById(id: Int): WiseSaying?
+    fun delete(wiseSaying: WiseSaying)
+    fun clear()
 }
